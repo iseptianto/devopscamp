6b8bd90a14957ef0f459db3c794209f1e3ceff2a
iseptianto <indraseptianto17@gmail.com>
2025-08-03 22:04:13 +0700

    update docker-compuse

--- model/model_tourism/register_model.py
+++ model/model_tourism/register_model.py
@@ -1,9 +1,9 @@
 import mlflow
 import os
+os.environ['MLFLOW_S3_ENDPOINT_URL'] = 'http://mlflow_minio:9000'
+os.environ['AWS_ACCESS_KEY_ID'] = 'minioadmin'
+os.environ['AWS_SECRET_ACCESS_KEY'] = 'minioadmin'
 
-os.environ['MLFLOW_S3_ENDPOINT_URL'] = "http://mlflow_minio:9000"
-os.environ['AWS_ACCESS_KEY_ID'] = "minioadmin"
-os.environ['AWS_SECRET_ACCESS_KEY'] = "minioadmin"
 
 mlflow.set_tracking_uri("http://mlflow_server:5001")
 mlflow.set_experiment("tourism-indonesia")
@@ -13,3 +13,13 @@ with mlflow.start_run():
         mlflow.log_artifact(file)
 
     mlflow.log_metric("dummy_accuracy", 0.9)
+
+import boto3
+
+boto3.client(
+    's3',
+    endpoint_url='http://mlflow_minio:9000',
+    aws_access_key_id='minioadmin',
+    aws_secret_access_key='minioadmin',
+    region_name='us-east-1'  # bebas
+)
